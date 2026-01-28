db354751eec4abd231ddf9b76797d9cedcc67249
Meetric <73554599+meetric1@users.noreply.github.com>
2026-01-27 22:07:50 -0500

    remove all terrain references
    terrain is not infmap specific
    
    https://github.com/meetric1/XalphoxTerrain

--- image_reader/README.md
+++
@@ -1,3 +0,0 @@
-# image_reader
-
-A simple serverside .dll to decode and sample image data
--- image_reader/compile_msvc.bat
+++
@@ -1,5 +0,0 @@
-mkdir build-release
-cd ./build-release
-cmake -G "Visual Studio 17 2022" -DCMAKE_BUILD_TYPE=Release ../ && ninja
-
-pause
